fa11977da0f9260b0a5918b7a8e2a2d527d36121
if21b021@technikum-wien.at <if21b021@technikum-wien.at>
2025-03-13 07:31:26 +0100

    duplicate functions for array elements are created (without parameter, with index, and with object and index as parameters)

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,31 +1,102 @@
 import { Component } from '@angular/core';
 
+export class CourseModel
+{
+  id: number;
+  name: string;
+
+  // constructor(id: number = 0, name: string = '') {
+  //   this.id = id;
+  //   this.name = name;
+  // }
+
+  constructor(course:CourseModel){
+    this.id = course.id;
+    this.name = course.name;
+  }
+
+}
+
+
+
+
 @Component({
   selector: 'courses',
-  template: `
-             <p>courses works!</p>
-             <input [(ngModel)]="email" (keyup.enter) ="onKeyUp()">
-
-`,
-
-
-                styleUrl: './courses.component.css'
+  templateUrl: "./courses.component.html",
+  styleUrl: './courses.component.css',
 })
 export class CoursesComponent {
 
-  email:string = "me@example.com";
+  email: string = "me@example.com";
+
+  courseName: string ="";
+  courses : CourseModel[] = [
+    { id: 1, name: 'Deutsch' },
+    { id: 2, name: 'English' },
+    { id: 3, name: 'Arabic ' },
+  ];  // Array of objects with id and name properties
+
+  addCourse()
+
+    {
+      let course = new CourseModel({ id: this.courses.length + 1, name: this.courseName });
+      this.courses.push(course);
+      this.courseName = "";
+
+    }
+
+    // ############# duplicate with an index as a parameter ##############
+
+    // duplicateCourse(index: number)
+    // {
+    //   let course = new CourseModel( this.courses.length +1, this.courses[index].name);
+    //   this.courses.splice(index+1,0,course);
+    // }
+
+    // ######### duplicate with a course object as a parameter #############
+    // duplicateCourse(course: CourseModel) // but there is a problem with id of the duplicated course
+    // {
+    //   let duplicatedCourse = new CourseModel(course);
+    //   let index : number  = this.courses.findIndex(c => c.id === course.id);
+    //   course.id = this.courses.length + 1;
+    //   this.courses.splice(index,0,duplicatedCourse);
+
+    // }
 
 
-  // updateEmail(event: Event) {
-  //   const inputElement = event.target as HTMLInputElement; // ✅ Type assertion
-  //   this.email = inputElement.value;
-  //   this.onKeyUp();
-  // }
-  onKeyUp() // (keyup.enter) ="updateEmail($event)"
+    //dupicate with a course object as a parameter and index as a parameter
+
+    duplicateCourse(course: CourseModel, index: number)
+    {
+      let duplicatedCourse = new CourseModel(course);
+      duplicatedCourse.id = this.courses.length + 1;
+      this.courses.splice(index+1,0,duplicatedCourse);
+    }
+
+
+ get allCourses() {
+    return this.courses;
+  }
+
+  onSubmit()
   {
-    //console.log("Enter key pressed! Current email:",this.email);
-    alert("Enter key pressed! Current email:"+this.email);
+    console.log(this.courses);
+
   }
 
 
-}
+    // updateEmail(event: Event) {
+    //   const inputElement = event.target as HTMLInputElement; // ✅ Type assertion
+    //   this.email = inputElement.value;
+    //   this.onKeyUp();
+    // }
+    onKeyUp() // (keyup.enter) ="updateEmail($event)"
+    {
+      //console.log("Enter key pressed! Current email:",this.email);
+      alert("Enter key pressed! Current email:" + this.email);
+    }
+
+
+  }
+
+
